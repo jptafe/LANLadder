84509a121ebdaca341e2cffbfbb45b8f919c82dc
Frag Spawn <frag.spawn@gmail.com>
2018-06-11 18:15:37 +1000

    team card

--- teamcard.php
+++ teamcard.php
@@ -1,22 +1,47 @@
 <?php
 
+    include 'functions.php';
+
+    $conn = dbconnect();
+
+    $getTotalTeamWins = "SELECT count(*) AS total_win 
+        FROM played_match
+        WHERE (team_a_id = " . (int)$_GET['teamid'] . " OR team_b_id = " . (int)$_GET['teamid'] . ") AND
+            winning_team_id = " . (int)$_GET['teamid'];
+    $stmt = $conn->prepare($getTotalTeamWins);
+    $stmt->execute();
+    $total_wins = $stmt->fetch(PDO::FETCH_ASSOC);
+    $total_wins = $total_wins['total_win']; 
+
+    $getTotalTeamLosses = "SELECT count(*) AS total_loss 
+        FROM played_match
+        WHERE (team_a_id = " . (int)$_GET['teamid'] . " OR team_b_id = " . (int)$_GET['teamid'] . ") AND
+            losing_team_id = " . (int)$_GET['teamid'];
+    $stmt = $conn->prepare($getTotalTeamLosses);
+    $stmt->execute();
+    $total_loss = $stmt->fetch(PDO::FETCH_ASSOC);
+    $total_loss = $total_loss['total_loss'];
+
+    $getTeamDetails = "SELECT * FROM team WHERE id = " . (int)$_GET['teamid'];
+    $stmt = $conn->prepare($getTeamDetails);
+    $stmt->execute();
+    $team_details = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $getLadders = "SELECT * FROM ladder";
+    $stmt = $conn->prepare($getLadders);
+    $stmt->execute();
+    $ladders = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
     $getTeamMembers = "SELECT player.name 
         FROM player, team
-        WHERE team.id = " . $_GET['teamid'] . " AND 
-        WHERE team.id = player.team_id;";
+        WHERE team.id = " . (int)$_GET['teamid'] . " AND 
+        team.id = player.team_id;";
+    $stmt = $conn->prepare($getTeamMembers);
+    $stmt->execute();
+    $members = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $getTotalTeamWins = "SELECT count(*)
-        FROM played_match
-        WHERE (team_a_id = " . $_GET['teamid'] . " OR team_b_id = " . $_GET['teamid'] . ") AND
-            winning_team_id = " . $_GET['teamid'];
-
-    $getTotalTeamLosses = "SELECT count(*)
-        FROM played_match
-        WHERE (team_a_id = " . $_GET['teamid'] . " OR team_b_id = " . $_GET['teamid'] . ") AND
-            losing_team_id = " . $_GET['teamid'];
-
-    $getLadders = "";
-    $getLadders = "";
+    echo '<div class=>';
+    echo '</div>';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +49,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>LANLAdder HOME</title>
+  <title>Team Card</title>
   <script src="js/code.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="css/style.css" type="text/css">
@@ -49,34 +74,47 @@
     <p>Create Matches</p>
     <p>Create Ladder</p>
   </nav>
-  <section class="container">
+  <section class="container" style="background-color: <?php echo $team_details['color']; ?>">
     <div class="title">
-      <h1>Blue Bleechers</h1></div>
+    <h1><?php echo $team_details['team_name']; ?></h1></div>
     <div class="detailedteamcard">
       <div class="teamcard">
-        <img src="img/bluebleechers.jpg" height="64" width="64" alt="teams logo">
-        <p>Team Size - (count members)</p>
-        <p class="teammemname">Team Members Names: Example, Example, Example</p>
+      <img src="img/<?php echo $team_details['image'] ?>" height="64" width="64" alt="teams logo">
+        <p><?php echo count($members) . ' - '; foreach($members as $member) { echo $member['name'] . ' '; } ?></p>
+
         <div class="teamstats">
-          <div class="tf2stats">
-            <img src="img/tf2.png" alt="TF2 Logo">
-            <p>W: 0</p>
-            <p>L: 0</p>
+<?php 
+    foreach ($ladders as $ladder) {
+
+        $ladderLoss = "SELECT count(*) AS loss 
+            FROM played_match
+            WHERE ladder_id = " . $ladder['id'] . " AND 
+            losing_team_id = " . (int)$_GET['teamid'];
+
+            $stmt = $conn->prepare($ladderLoss);
+            $stmt->execute();
+            $losses = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $ladderWins = "SELECT count(*) AS win  
+            FROM played_match
+            WHERE ladder_id = " . $ladder['id'] . " AND 
+            winning_team_id = " . (int)$_GET['teamid'];
+
+            $stmt = $conn->prepare($ladderWins);
+            $stmt->execute();
+            $wins = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+          <div>
+            <img src="img/<?php echo $ladder['image']; ?>" alt="TF2 Logo">
+            <p>W: <?php echo $wins['win'] ?></p>
+            <p>L: <?php echo $losses['loss'] ?></p>
           </div>
-          <div class="csgostats">
-            <img src="img/csgo.jpg" alt="csgo Logo">
-            <p>W: 0</p>
-            <p>L: 0</p>
-          </div>
-          <div class="rlstats">
-            <img src="img/rocketleague.jpeg" alt="rl Logo">
-            <p>W: 0</p>
-            <p>L: 0</p>
-          </div>
+<?php } ?>
           <div class="totalstats">
             <h2>Total</h2>
-            <p>W: 0</p>
-            <p>L: 0</p>
+            <p>W: <?php echo $total_wins; ?></p>
+            <p>L: <?php echo $total_loss; ?></p>
           </div>
         </div>
       </div>
