9508eb9aaca7dde1690a7241bb8f70a9fb3bb9ed
Ethan <ethouley@gmail.com>
2018-06-11 17:37:23 +1000

    Login and Register, Will be hashing password when user register.

--- control/registerprocess.php
+++ control/registerprocess.php
@@ -2,10 +2,10 @@
   require_once '../functions.php';
   if ($_POST['psw'] = $_POST['psw-repeat']) {
     $Username = clean($_POST['Name']);
-    $UserPassword = clean($_POST['pws']);
+    $UserPassword = clean($_POST['psw']);
     $HashedPassword = password_hash($UserPassword, PASSWORD_DEFAULT);
     $_SESSION['user'] = $Username;
-    $_SESSION['UserPrivileges'] = 1;
+    $_SESSION['UserPrivileges'] = 0;
     header('location: ../index.php');
   } else {
     $_SESSION['error_message'] = 'Your password do not match';
