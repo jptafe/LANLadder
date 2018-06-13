3d73c02d3660a196276e7c447c9a986f0183dc8b
Frag Spawn <frag.spawn@gmail.com>
2018-06-13 20:21:12 +1000

    localise jQuery & Materialize

--- functions.php
+++ functions.php
@@ -110,11 +110,11 @@
       <title>LANLAdder HOME</title>
       <script src="js/code.js"></script>
       <!-- Compiled and minified CSS -->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+      <link rel="stylesheet" href="css/materialize.css">
 
       <!-- Compiled and minified JavaScript -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-      <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+      <script src="js/materialize.js"></script>
+      <script src="js/jquery-3.3.1.min.js"></script>
       <link rel="stylesheet" href="css/style.css" type="text/css">
     </head>
     <?php
