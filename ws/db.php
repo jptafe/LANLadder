d2ecda1adabdb78b3a3fab44978a720444d9019c
EC2 Default User <ec2-user@ip-172-31-0-94.us-west-2.compute.internal>
2019-08-15 23:12:34 +0000

    aws configs

--- ws/db.php
+++ ws/db.php
@@ -3,8 +3,8 @@
         private $conn;
         public function __construct() {
             try {
-                $this->conn = new PDO("mysql:host=localhost;dbname=LANLadder", 'root','');
-                //$this->conn = new PDO("mysql:host=localhost;dbname=LANLadder", 'root','RRU7xecj8E2EqL');
+                //$this->conn = new PDO("mysql:host=localhost;dbname=LANLadder", 'root','');
+                $this->conn = new PDO("mysql:host=aa1rdt1oikf2nc.cqbylsinlsjf.us-west-2.rds.amazonaws.com;port=3306;dbname=LANLadder", 'adminuser','thisisapassword');
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // DEBUG
             }
             catch(PDOException $e) {
